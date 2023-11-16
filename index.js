0daeb053db496c6620b9ea8e8baac9f03bd49308
Anyona-00 <125492744+Anyona-00@users.noreply.github.com>
2023-11-16 09:40:43 +0300

    cleaning

--- index.js
+++
@@ -1,8 +0,0 @@
-// index.js
-
-const menuToggle = document.getElementById("menu-toggle");
-const navBar = document.getElementById("nav-bar");
-
-menuToggle.addEventListener("click", () => {
-  navBar.classList.toggle("show-menu");
-});
